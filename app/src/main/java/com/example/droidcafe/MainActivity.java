a96f243dd2cf221cce6bf7c1a166883382a31e32
daniel <danilaia12@hotmail.com>
2019-12-11 20:25:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.droidcafe;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void displayToast(String message) { Toast.makeText(getApplicationContext(),
+            message,
+            Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        showFoodOrder(getString(R.string.froyo_order_message));
+    }
+
+    public void apantalla1(View view){
+        showDonutOrder(view);
+    }
+
+    public void apantalla2(View view){
+        showIceCreamOrder(view);
+    }
+
+    public void apantalla3(View view){
+        showFroyoOrder(view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showFoodOrder(String message) {
+        displayToast(message);
+        startActivity(new Intent(this, pantalla1.class));
+    }
+}
